e1f4661ddccd0ac6a96648d7d3a42fd380e98c77
gumarm <guillaume.martin555@gmail.com>
2016-11-07 21:41:51 -0500

    Changed test case for custom validators.

--- test/validators.spec.js
+++ test/validators.spec.js
@@ -3,11 +3,11 @@ import { should } from 'should';
 import { runValidators } from '../dist/runValidators';
 
 describe('run validators', () => {
-  it('should run validator', (done) => {
+  it('should run all custom validators', (done) => {
     try {
       runValidators({
         parameters: [{
-          in: 'body', name: 'testValue', validators: [(value) => {
+          in: 'body', name: 'testValue', validators: [() => { }, (value) => {
             if (!value) {
               throw new Error('string is empty');
             }
